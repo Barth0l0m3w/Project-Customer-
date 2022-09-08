d8594d689854206eb55262916798b168f7dd9fa7
Nathalie <xnhogenbergx@gmail.com>
2022-09-07 18:48:45 -0700

    clicking 'buildings'
    redirecting to different scenes when clicking on the buildings

--- Assets/scripts/InteractableObjectSelection.cs
+++ Assets/scripts/InteractableObjectSelection.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class InteractableObjectSelection : MonoBehaviour
 {
     private Color startingColor;
     private Renderer renderers;
+    public GameObject buildingOne;
+    public GameObject buildingTwo;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +27,29 @@ public class InteractableObjectSelection : MonoBehaviour
     private void OnMouseEnter()
     {
         renderers.material.color = Color.red;
+        if (gameObject.name.Equals("buildingOne"))
+        {
+            Debug.Log("clicked building one");
+        }
     }
 
     private void OnMouseExit()
     {
         renderers.material.color = startingColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameObject.name.Equals("BuildingOne"))
+        {
+            Debug.Log("clicked building one");
+            SceneManager.LoadScene("insideBuildingOne");
+        }
+
+        if (gameObject.name.Equals("BuildingTwo"))
+        {
+            Debug.Log("clicked building two");
+            SceneManager.LoadScene("insideBuildingTwo");
+        }
     }
 }
